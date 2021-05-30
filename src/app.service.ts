6378223b25dbdea4b2f0ab39722e182854b720b2
SPbryan007 <bryan.guns30@gmail.com>
2021-05-29 23:13:04 -0400

    Hellow world changed

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hola mundo a todos';
+    return 'Hola mundo a todos desde ecs';
   }
 }
