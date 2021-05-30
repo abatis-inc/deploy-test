63ec1b7e9358273caed2d0ed8fd52822bdcce6bc
SPbryan007 <bryan.guns30@gmail.com>
2021-05-29 23:18:37 -0400

    aws en la nube added

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hola mundo a todos desde ecs';
+    return 'Hola mundo a todos desde ecs aws en la nube';
   }
 }
